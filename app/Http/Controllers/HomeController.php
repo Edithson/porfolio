1fb23c037f7292668e43ac9db63eab54e8f920f6
Edithson <moafogaus@gmail.com>
2026-05-16 18:28:23 +0100

    Update : Initial centralization of homepage data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index(){
+        $projects = [
+            [
+                "id" => 1,
+                "name" => "Dossiers RH MINFI",
+                "description" => "Conception et développement d'un programme de gestion des dossiers du personnel pour le Ministère des Finances. Centralisation et sécurisation de l'information pour plusieurs centaines d'agents.",
+                "image" => "img/projects/rh-minfi.png",
+                "test_link" => "",
+                "git_link" => "",
+                "tags" => ["Architecture métier", "PostgreSQL", "Laravel"],
+                "categort" => "Logiciel métier",
+            ]
+        ];
+
+        $infos = [
+            "email" => "moafogaus@gmail.com",
+            "logo" => "img/logo.png",
+            "adr_git" => "https://github.com/Edithson",
+        ];
+        return view('portfolio', compact('projects','infos'));
+    }
 }
